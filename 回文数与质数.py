1c72d61bc4ce7c4fccaa6b484e33715084a88cb8
python-lz <53743121+python-lz@users.noreply.github.com>
2019-08-08 21:14:44 +0800

    回文数与质数

---
+++ 回文数与质数.py
@@ -0,0 +1,18 @@
+result = []
+for a in range(1,100000):
+    b = str(a)
+    list1 = list(b)
+    list2 = list1.copy()
+    list2.reverse()
+    if list1 == list2:
+        result.append(b)
+result2 = []
+#筛选质数
+for n in result:
+    s = int(n)
+    for num in range(2,s):
+        if s%num ==0:
+            break
+        if num == s-1:
+            result2.append(n)
+print(result2)
